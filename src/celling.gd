b64adb259f4397dff6dac7e99dd6b2359b54b215
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-06-08 05:00:38 +0700

    implement bounce on top and bottom

--- src/celling.gd
+++ src/celling.gd
@@ -1,7 +1,16 @@
 extends Area2D
 
+var _ball_dir = 0
+
 func _ready() -> void:
+    if name == "Top":
+        _ball_dir = 1
+    else:
+        _ball_dir = -1
     pass
 
-func _process(delta: float) -> void:
-   pass
+
+func _on_Area2D_area_entered(area: Area2D) -> void:
+    if area.name == "Ball":
+        area._direction = Vector2(randf() * 2 - 1, _ball_dir).normalized()
+    pass
