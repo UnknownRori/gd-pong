aa0d100ac79a0e01ef8672af944c302699d7d96f
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-06-07 16:38:30 +0700

    add basic ball movement

--- src/ball.gd
+++ src/ball.gd
@@ -1,7 +1,21 @@
-extends Sprite
+extends Area2D
+
+onready var _initial_pos = position
+
+const BALL_SPEED  = 200
+var   _speed      = BALL_SPEED
+var   _direction  = Vector2.LEFT
 
 func _ready() -> void:
+    
     pass
 
 func _process(delta: float) -> void:
-   pass
+    _speed += delta * 2
+    position += BALL_SPEED * delta * _direction
+    pass
+
+func reset():
+    _direction = Vector2.LEFT
+    position = _initial_pos
+    _speed = BALL_SPEED
--- src/paddle.gd
+++ src/paddle.gd
@@ -1,7 +1,18 @@
-extends Sprite
+extends Area2D
+
+var _ball_dir = 0
 
 func _ready():
-    pass 
+    if name == "Left":
+        _ball_dir = -1
+    else:
+        _ball_dir = 1
 
 func _process(delta):
+    pass
+
+
+func _on_Area2D_area_entered(area:Area2D) -> void:
+    if area.name == "Ball":
+        area._direction = Vector2(_ball_dir, randf() * 2 - 1).normalized()
     pass
